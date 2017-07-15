8bc4573e5a04cc151fb7936e84b5cbbcff721615
dreyes147 <davidre701@gmail.com>
2017-07-15 10:56:36 -0600

    Agregar archivos de proyecto.

---
+++ Interfaz/Login.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Interfaz
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.gboInformacion = new System.Windows.Forms.GroupBox();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.ptbImagen = new System.Windows.Forms.PictureBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.Panel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gboInformacion.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImagen)).BeginInit();
+            this.Panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(167, 189);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.PasswordChar = '●';
+            this.txtContraseña.Size = new System.Drawing.Size(164, 20);
+            this.txtContraseña.TabIndex = 1;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(167, 163);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(164, 20);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // gboInformacion
+            // 
+            this.gboInformacion.Controls.Add(this.lblContraseña);
+            this.gboInformacion.Controls.Add(this.lblUsuario);
+            this.gboInformacion.Controls.Add(this.ptbImagen);
+            this.gboInformacion.Controls.Add(this.btnCancelar);
+            this.gboInformacion.Controls.Add(this.txtContraseña);
+            this.gboInformacion.Controls.Add(this.btnAceptar);
+            this.gboInformacion.Controls.Add(this.txtUsuario);
+            this.gboInformacion.Location = new System.Drawing.Point(12, 82);
+            this.gboInformacion.Name = "gboInformacion";
+            this.gboInformacion.Size = new System.Drawing.Size(439, 305);
+            this.gboInformacion.TabIndex = 5;
+            this.gboInformacion.TabStop = false;
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContraseña.Location = new System.Drawing.Point(78, 190);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(83, 16);
+            this.lblContraseña.TabIndex = 6;
+            this.lblContraseña.Text = "Contraseña:";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(78, 163);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(60, 16);
+            this.lblUsuario.TabIndex = 5;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // ptbImagen
+            // 
+            this.ptbImagen.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ptbImagen.Image = global::Interfaz.Properties.Resources.user_icon_icons_com_66546__1_;
+            this.ptbImagen.Location = new System.Drawing.Point(165, 19);
+            this.ptbImagen.Name = "ptbImagen";
+            this.ptbImagen.Size = new System.Drawing.Size(120, 119);
+            this.ptbImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbImagen.TabIndex = 0;
+            this.ptbImagen.TabStop = false;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Image = global::Interfaz.Properties.Resources.salir_de_mi_perfil_icono_3964_16;
+            this.btnCancelar.Location = new System.Drawing.Point(236, 224);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 4;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Image = global::Interfaz.Properties.Resources.aceptar_icono_6494_16__1_;
+            this.btnAceptar.Location = new System.Drawing.Point(147, 224);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 3;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // Panel
+            // 
+            this.Panel.BackColor = System.Drawing.Color.Transparent;
+            this.Panel.BackgroundImage = global::Interfaz.Properties.Resources.background_swirl1;
+            this.Panel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Panel.Controls.Add(this.panel1);
+            this.Panel.Location = new System.Drawing.Point(1, 1);
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(470, 75);
+            this.Panel.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = global::Interfaz.Properties.Resources.vector_film_strip_G1ZJ7xPu;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(363, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(107, 74);
+            this.panel1.TabIndex = 0;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(471, 399);
+            this.Controls.Add(this.Panel);
+            this.Controls.Add(this.gboInformacion);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.gboInformacion.ResumeLayout(false);
+            this.gboInformacion.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImagen)).EndInit();
+            this.Panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptbImagen;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.GroupBox gboInformacion;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Panel Panel;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ Interfaz/Login.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaz
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        #region Declaración de Variables
+
+        #endregion
+
+        #region Declaracion de Métodos
+
+        #endregion
+
+        #region Declaración de Eventos
+        private void btnCancelar_Click(object sender, EventArgs e)
+            {
+                Close();
+            }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+        #endregion
+
+
+    }
+}
